20e8c36679190a14b88edb8bca208cf3de1eb1bf
sour-soup <kiziyepi8@gmail.com>
2025-05-23 01:40:56 +0400

    initial commit

---
+++ bim-bim-backend/src/test/java/org/soursoup/bimbim/BimBimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soursoup.bimbim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimBimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
