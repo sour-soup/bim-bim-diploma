fad6bb1e94c5472edb4191c854da4aacb49e69e5
sour-soup <kiziyepi8@gmail.com>
2025-05-24 20:43:16 +0400

    remove redundant repository methods

--- bim-bim-backend/src/main/java/org/soursoup/bimbim/repository/UserCategoryRepository.java
+++ bim-bim-backend/src/main/java/org/soursoup/bimbim/repository/UserCategoryRepository.java
@@ -6,12 +6,9 @@ import org.soursoup.bimbim.entity.UserCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserCategoryRepository extends JpaRepository<UserCategory, Long> {
     Optional<UserCategory> findByUserAndCategory(User user, Category category);
-
-    List<UserCategory> findAllByCategory(Category category);
 }
